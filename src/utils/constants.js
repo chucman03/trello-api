cf38a548c12154f3d2b652cb0e1fd838ce543197
chucman03 <chucman2003@gmail.com>
2025-04-29 18:38:16 +0700

    update cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 export const WHITELIST_DOMAINS = [
-    'http://localhost:5173'
+    // 'http://localhost:5173'
 ]
 export const BOARD_TYPES = {
     PUBLIC: 'public',
